bf40725c9b959ef57202ba11c4b3fee43141392e
De.Li <De.Li@Besttone888>
2015-05-29 17:16:46 +0800

    init local

---
+++ portal/src/main/java/com/lee/portal/dao/UserDao.java
@@ -0,0 +1,11 @@
+package com.lee.portal.dao;
+
+import org.springframework.stereotype.Repository;
+
+import com.lee.commons.dao.BaseDao;
+import com.lee.portal.po.UserPO;
+
+@Repository("userDao")
+public class UserDao extends BaseDao<UserPO, String> {
+
+}
---
+++ portal/src/main/java/com/lee/portal/service/UserService.java
@@ -0,0 +1,8 @@
+package com.lee.portal.service;
+
+import com.lee.commons.service.BaseService;
+import com.lee.portal.po.UserPO;
+
+public interface UserService extends BaseService<UserPO,String>{
+	
+}
